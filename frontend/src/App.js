2de4b402d6f1c05411c87a0b48f0df78642e8e7e
Emmah Moraa <alphagal254@gmail.com>
2022-06-29 15:56:54 +0300

    homepage product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { Container } from 'react-bootstrap';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+
 function App() {
 	return (
 		<>
-			<h1>Welcome to Alphagal Collections</h1>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<HomePage />
+				</Container>
+			</main>
+			<Footer />
 		</>
 	);
 }
