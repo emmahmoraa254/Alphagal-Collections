93365514dfefd6b871c3b79bd7f884319b909735
Emmah Moraa <alphagal254@gmail.com>
2022-06-29 17:53:11 +0300

    product details page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<main className='py-3'>
 				<Container>
 					<Route path='/' component={HomePage} exact />
+					<Route path='/product/:id' component={ProductPage} />
 				</Container>
 			</main>
 			<Footer />
