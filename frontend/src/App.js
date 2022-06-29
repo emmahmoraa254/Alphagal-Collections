ce53e5b718ef4940889c21abe66061f0c00fd240
Emmah Moraa <alphagal254@gmail.com>
2022-06-29 16:55:50 +0300

    rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
 import { Container } from 'react-bootstrap';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 
 function App() {
 	return (
-		<>
+		<Router>
 			<Header />
 			<main className='py-3'>
 				<Container>
-					<HomePage />
+					<Route path='/' component={HomePage} exact />
 				</Container>
 			</main>
 			<Footer />
-		</>
+		</Router>
 	);
 }
 
